943e7f3fa64187792aca78441ecee9abe4d0ba36
neb <10528790@my.uvu.edu>
2021-05-26 21:28:15 -0600

    using the rest operator to pass arbitrary number of args to method

---
+++ let_const_scope/seven.js
@@ -0,0 +1,12 @@
+// rest operator to pass arbitrary number of method args
+let addUp = (...nums) => {
+	// rest operator creates an array of values
+	console.log(nums);
+	let sum = 0;
+  for (let num of nums) {
+  	sum += num;
+  }
+  return sum;
+};
+
+console.log(addUp(21,14,18,7));
